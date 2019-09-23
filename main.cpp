6f230146087b10213b773b7eb0c740da3fc36b04
jbr2000 <jbrupert@mail.csuchico.edu>
2019-09-23 16:08:42 -0700

    program asks for name

--- main.cpp
+++ main.cpp
@@ -7,9 +7,13 @@ using namespace std;
 int main()
 {
   //declare variables
-
+string first;
+string second;
   //get user input
-
+cout<<"What is your first name?"<<endl;
+cin>>first;
+cout<<"What is your last name?"<<endl;
+cin>>second;
   //tell fortune
 
   return 0;
